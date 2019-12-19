6f7cacff26f82a43caf16aca1c2281203105ef14
LORDBABUINO <jorge.x7@gmail.com>
2019-12-19 03:01:14 -0300

    Search professor by name

--- src/app/controllers/ProfessorController.js
+++ src/app/controllers/ProfessorController.js
@@ -4,8 +4,10 @@ import Professor from '../models/Professor'
 
 class ProfessorController {
   async index(req, res) {
+    const { name } = req.query
     const Professores = Parse.Object.extend('Professores')
     const query = new Parse.Query(Professores)
+    if (name) query.matches('nome', new RegExp(`^${name}`, 'i'))
     const results = await query.find()
     return res.json(results.map(Professor))
   }
