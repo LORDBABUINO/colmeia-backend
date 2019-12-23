1650161293a8d1b52b9b5267e5a6579f082df757
LORDBABUINO <jorge.x7@gmail.com>
2019-12-23 18:07:35 -0300

    Updates env variables

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,7 +1,10 @@
 import 'dotenv/config'
 
 export default (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL)
+  res.header(
+    'Access-Control-Allow-Origin',
+    `${process.env.FRONTEND_URL}:${process.env.FRONTEND_PORT}`
+  )
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
