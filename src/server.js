3aa8fc65a21f92426731801376b2167ce62e42c6
LORDBABUINO <jorge.x7@gmail.com>
2019-12-23 17:49:46 -0300

    Revert "Configurates static server"
    This reverts commit 2195f3be70e8f2f496221d1de44d11c6a6034795.

--- src/server.js
+++ src/server.js
@@ -1,7 +1,3 @@
-import 'dotenv/config'
 import app from './app'
-import staticServer from './StaticServer'
 
-app.listen(process.env.BACKEND_PORT)
-if (process.env.NODE_ENV === 'production')
-  staticServer.listen(process.env.FRONTEND_PORT)
+app.listen(3333)
